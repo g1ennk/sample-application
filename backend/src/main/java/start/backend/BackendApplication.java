9631c1a8b939c209ad832206e82c021a6f163108
KIM MINSEOK <mskim.glenn@gmail.com>
2025-03-26 23:33:59 +0900

    test: CodeDeploy test for backend

--- backend/src/main/java/start/backend/BackendApplication.java
+++ backend/src/main/java/start/backend/BackendApplication.java
@@ -12,4 +12,4 @@ public class BackendApplication {
 
 }
 
-// CodeDeploy 테스트 12
+// CodeDeploy 테스트 13
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Logging in to Amazon ECR..."
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 221082195716.dkr.ecr.ap-northeast-2.amazonaws.com
 
-echo "Pulling latest image..."
-docker pull 221082195716.dkr.ecr.ap-northeast-2.amazonaws.com/weekly-backend-app:latest
+aws ecr get-login-password --region ap-northeast-2 \
+| docker login --username AWS --password-stdin 221082195716.dkr.ecr.ap-northeast-2.amazonaws.com
+
+docker pull 221082195716.dkr.ecr.ap-northeast-2.amazonaws.com/weekly-repo:latest
