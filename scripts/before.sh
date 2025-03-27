9e0192d43e3e5f97c648f1dcc4e7911892df6944
KIM MINSEOK <mskim.glenn@gmail.com>
2025-03-27 21:08:14 +0900

    fix: Update scripts

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 docker system prune -af --volumes 2>/dev/null || true
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 docker stop weekly-backend 2>/dev/null || true
 docker rm weekly-backend 2>/dev/null || true
